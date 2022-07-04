8efe42b57597634cd17b9a7afdadcff9e0d92355
Giga_sat <90619014+GigaSatx@users.noreply.github.com>
2022-07-04 18:45:44 +0530

    Project build final

---
+++ build/precache-manifest.7c1197e9c04782a9953f7fb03fe2d24f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "14713112e5fc3e66ecf5",
+    "url": "/static/css/main.771bb1b1.chunk.css"
+  },
+  {
+    "revision": "14713112e5fc3e66ecf5",
+    "url": "/static/js/main.14713112.chunk.js"
+  },
+  {
+    "revision": "a46c3e295f5ff88ce5c1",
+    "url": "/static/js/1.a46c3e29.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f67bf5886654f658c91a4d067c61ccd0",
+    "url": "/index.html"
+  }
+];
